d6a49709e9c9c307af301e23f317f0ab092efa86
陳冠邑 <100064250+chenkuanyi0531@users.noreply.github.com>
2024-03-05 15:23:56 +0800

    Update lineOpenAI.py

--- lineOpenAI.py
+++ lineOpenAI.py
@@ -6,11 +6,11 @@ import json
 with open("model_config.json") as f:  
     model_list = json.load(f)  
   
-openai.api_key = model_list[0]['key']  
-openai.api_base = model_list[0]['endpoint']  # 您的 endpoint 應該類似於 https://YOUR_RESOURCE_NAME.openai.azure.com/  
+openai.api_key =os.getenv("AZURE_OPENAI_API_KEY")
+openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")  # 您的 endpoint 應該類似於 https://YOUR_RESOURCE_NAME.openai.azure.com/  
 openai.api_type = 'azure'  
-openai.api_version = model_list[0]['endpoint']  # 這可能會在未來變更  
-  
+openai.api_version = os.getenv("AZURE_OPENAI_API_VERSION")  # 這可能會在未來變更  
+model =os.getenv("AZURE_OPENAI_MODEL")
   
 # 初始化一個空的訊息歷史列表  
 messages_history = [  
@@ -23,7 +23,7 @@ def GPT_response(text):
     messages_history.append({"role": "user", "content": text})  
       
     response = openai.ChatCompletion.create(  
-        engine=model_list[0]['deployment'],  
+        engine=model,  
         messages=messages_history,  # 將帶有歷史紀錄的列表發送給模型  
         temperature=0.9,  
         max_tokens=1000,  
